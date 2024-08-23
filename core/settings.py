c65718c1d0be990e925865e93df6f0297550efa3
ali-jee-786 <786syedalihussain@gmail.com>
2024-08-23 21:44:44 +0500

    updated

--- core/settings.py
+++ core/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = ["static"]
+# STATICFILES_DIRS = ["static"]
 
 
 # Default primary key field type
